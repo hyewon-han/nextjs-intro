b7038cff200eb374627ea19c2aa2c8cb87a32399
hyewon-han <hw1635@hanmail.net>
2023-12-22 22:57:19 +0900

    init

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import NavBar from "@/components/NavBar";
+import Seo from "@/components/Seo";
+import React from "react";
+const about = () => {
+  return (
+    <div>
+      <Seo title="About" />
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default about;
